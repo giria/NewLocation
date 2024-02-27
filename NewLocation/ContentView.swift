16327c8e4f15e529860ec7fdcb5787321fc3bbd1
giria <joan@giria.com>
2024-02-27 21:55:49 +0000

    Initial Commit

---
+++ NewLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewLocation
+//
+//  Created by joan Barrull on 27/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewLocation/NewLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLocationApp.swift
+//  NewLocation
+//
+//  Created by joan Barrull on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
