cf1e985920954e7733307ae4efbfce9f058017ae
giria <joan@giria.com>
2024-02-28 08:14:02 +0000

    Initial commit

--- NewLocation/ContentView.swift
+++ NewLocation/ContentView.swift
@@ -1,24 +1,46 @@
-//
-//  ContentView.swift
-//  NewLocation
-//
-//  Created by joan Barrull on 27/02/2024.
-//
-
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @State var newlocationManager = NewLocationManager()
+   
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "location")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+                .padding()
+            
+            Text("New location manager: \(newlocationManager.location?.description ?? "No Location Provided!")")
         }
         .padding()
+       
+        .task {
+            try? await newlocationManager.requestUserAuthorization()
+            try? await newlocationManager.startCurrentLocationUpdates()
+            // remember that nothing will run here until the for try await loop finishes
+        }
     }
 }
 
-#Preview {
-    ContentView()
+
+@Observable
+class NewLocationManager {
+    var location: CLLocation? = nil
+    
+    private let locationManager = CLLocationManager()
+    
+    func requestUserAuthorization() async throws {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func startCurrentLocationUpdates() async throws {
+        for try await locationUpdate in CLLocationUpdate.liveUpdates() {
+            guard let location = locationUpdate.location else { return }
+
+            self.location = location
+        }
+    }
 }
